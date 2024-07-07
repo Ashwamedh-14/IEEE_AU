e254688b5b7a4b96f23e6c577376fe43173f2a04
Aswhamedh <113002273+Ashwamedh-14@users.noreply.github.com>
2024-07-07 13:40:05 +0530

    Added a .env file to save environment variables in. Removed cred.dat since it was no longer needed. Removed pickle import. Added os import. Restructured the try - except - else - finally block in main to make it cleaner

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #built in modules
-from pickle import load, dump
 from time import sleep
 import logging
 from logging import handlers
+from os import getenv
 
 #third party modules
 from mysql.connector import connect, errors
+from dotenv import load_dotenv
 
 #self defined modules
 import src.attendence as attendence
@@ -21,24 +22,6 @@ logger.addHandler(file_handle)
 logger.setLevel(logging.DEBUG)                                                  #Setting Log level
 logger.propagate = False
 
-
-def new_credential(cred_file) -> None:
-    '''
-    cred_file should be the buffered reader for cred.dat file which is opened in binary format with filemode 'wb'
-    '''
-    d = {}
-    print("Kindly enter all the details")
-    d["host"] = input("Enter the host of the database: ")
-    d["user"] = input("Enter the user of the database: ")
-    d["password"] = input("Enter the password of the database: ")
-    d["database"] = input("Enter the name of the database to be used: ")
-    dump(d, cred_file)
-    print("Credentials saved successfully")
-
-
-
-
-
 def main():
     logger.debug(f"Starting the program {__name__}")
     try:
@@ -47,61 +30,20 @@ def main():
         print("Connecting with database....")
         sleep(5)
 
+        load_dotenv()    #loading environment variables stored in .env into the running nevironment
+        logger.debug(f"Host : {getenv('HOST')}\nUser : {getenv('USER')}\nPassword : {getenv('PASSWORD')}\nDatabase : {getenv('DATABASE')}")
+
         #inputing database details from binary file
-        try:
-            flag = True                      # In case we meet an exception, we use this flag to skip code in finally block
+        flag = False
+        con = connect(
+            host = getenv('HOST'),
+            user = getenv('USER'),
+            password = getenv('PASSWORD'),
+            database = getenv('DATABASE')
+        )
 
-            logger.debug("Opening the file")
-            with open("cred.dat", 'rb') as myfile:
-                logger.debug(f"File opened {myfile.name}")
-                data: dict = load(myfile)               #data is stored in a dictionary
-
-                logger.debug("Setting up connection with IEEE Database")
-                con = connect(host = data['host'],
-                              user = data['user'],
-                              password = data['password'],
-                              database = data['database'])
-                logger.debug("Connection established")
-
-        #If the file does not exists
-        except FileNotFoundError as e:
-            flag = False
-            logger.error(f"The cred file was not there. Creating a new one...")
-            print("Looks like you don't have a credential file")
-            sleep(1)
-            print("We'll set one up for you")
-            sleep(1)
-
-            #creating file
-            with open("cred.dat", "wb") as myfile:
-                new_credential(myfile)
-            print("Credentials saved successfully")
-            print("If you want to resave the credentials. Simply delete the credential file, i.e., cred.dat")
-            print("Please restart the program")
-            return
-        
-        #In case the credentials are invalid
-        except errors.DatabaseError as e:
-            flag = False
-            print("It looks like the credentials stored are invalid.")
-            ch = input("Would you like to save again? (Y / N): ")
-            if ch == None:
-                return
-            elif ch.upper().strip() in ('Y', 'Yes'):
-                with open('cred.dat', 'wb') as myfile:
-                    new_credential(myfile)
-                print("Kindly restart the program")
-            else:
-                return
-
-        #To catch general exceptions
-        except Exception as e:
-            flag = False
-            logger.critical(f"Was not able to connect to database\n{e}", exc_info = True)
-            print(f"The following error occured:\n{repr(e)}") #To indicate what could have potentially caused the crash
-            return
         curr = con.cursor()
-        print("Connection established: ")
+        flag = True
 
         #Main menu of program
         while True: 
@@ -109,15 +51,14 @@ def main():
             print("Kindly enter anyone of the actions below:")
             print('1. Take attendence')
             print("2. Members Related")
-            print("3. Edit / Save credentials")
-            print('4. Exit')
+            print('3. Exit')
 
             while True:
                 #To input only correct values
                 try:
-                    ch = int(input("Enter your choice (1 - 4): "))
-                    if ch < 1 or ch > 4:
-                        print("Enter between 1 and 4 inclusive only")
+                    ch = int(input("Enter your choice (1 - 3): "))
+                    if ch < 1 or ch > 3:
+                        print("Enter between 1 and 3 inclusive only")
                     else:
                         print()
                         break
@@ -138,21 +79,27 @@ def main():
                 print() #To maintain some space between one section and another
                 logger.debug("Out of members file")
 
-            elif ch == 3:
-                logger.debug("Editing / Saving credential for database")
-                with open('cred.dat', 'wb') as myfile:
-                    new_credential(myfile)
-                print("To fully save the cred file, kindly exit the program and restart")
-
             else:
                 logger.debug("Closing Program")
-                print()
                 break
+
+
+    #In case the credentials are invalid
+    except errors.DatabaseError as e:
+        print("It looks like the credentials stored are invalid.")
+        print("Kindly check whether your environment varables are properly configured or not")
+        return
 
     except KeyboardInterrupt:
         logger.debug("KeyboardInterrupt was hit")
         con.rollback()
         print()
+
+    except Exception as e:
+        flag = False
+        logger.critical(f"Was not able to connect to database\n{e}", exc_info = True)
+        print(f"The following error occured:\n{repr(e)}") #To indicate what could have potentially caused the crash
+        return
 
     finally:
         if not flag:
